21e3e3129b741e4d34487cf00beaef8f36595640
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-31 11:04:43 +0700

    Class orang dengan 2 member

--- problemMultiwaris/problemMultiwaris.cpp
+++ problemMultiwaris/problemMultiwaris.cpp
@@ -5,7 +5,7 @@ class orang {
 public:
     int umur;
 
-    orang(int pUmur) :
+    orang(int pUmur) : 
         umur(pUmur)
     {
         cout << "orang dibuat dengan umur" << umur "\n" << endl; 
