195e6aeadab896c0dbd3382bc225ad8895722dcf
Iffatuz Zahra <iffatuz.zahra.ft22@mail.umy.ac.id>
2023-05-31 19:00:09 +0700

    menggunakan virtual method untuk memunculkan pesan dari lia dan joko

---
+++ Program MultiWaris/Program MultiWaris.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+    string nama;
+
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+    ~orang(){
+        cout << "orang dihapus\n" << endl;
+    }
+};
+
+class manusia {
+public:
+    string jenisKelamin;
+
+    manusia(string pJenisKelamin) :
+        jenisKelamin(pJenisKelamin) {
+        cout << "Manusia dibuat\n" << endl;
+    }
+    ~manusia() {
+        cout << "Manusia dihapus\n" << endl;
+    }
+};
+
+class pelajar :private manusia, public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pJenisKelamin, string pSekolah) :
+        orang(pNama),
+        manusia(pJenisKelamin),
+        sekolah(pSekolah) {
+        cout << "Pelajar dibuat\n" << endl;
+    }
+    ~pelajar(){
+        cout << "Pelajar dihapus\n" << endl;
+    }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + "dengan jenis kelamin" +jenisKelamin + "dari sekolah " + sekolah + "\n\n";
+    }
+};
+
+int main() {
+    pelajar siswa("andi laksono", "laki-laki", "SMAN 1 Bantul");
+    cout << siswa.perkenalan();
+
+    return 0;
+}
